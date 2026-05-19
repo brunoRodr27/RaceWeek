034c32e2a0b13f8edee315bbdd6f67e0a9ee120f
unknown <brunodasilvarodrigues.28@gmail.com>
2026-05-19 20:56:33 -0300

    Adicionando sessões

--- app/src/main/java/com/example/raceweek/domain/util/SessionLabelUtils.kt
+++ app/src/main/java/com/example/raceweek/domain/util/SessionLabelUtils.kt
@@ -6,8 +6,21 @@ fun String.toSessionLabel(): String = when (this.lowercase()) {
     "practicetwo"     -> "Treino Livre 2"
     "practicethree"   -> "Treino Livre 3"
     "qualifying"      -> "Classificação"
+    "qualifyingone"   -> "Classificação 1"
+    "qualifyingtwo"   -> "Classificação 2"
+    "qualifyingthree" -> "Classificação 3"
     "sprintqualifying"-> "Classificação - Sprint"
     "sprint"          -> "Corrida - Sprint"
     "race"            -> "Corrida"
+
+    // Treinos Indianapolis 500
+    "practiceone12"   -> "Treino Livre 1 (1/2)"
+    "practiceone22"   -> "Treino Livre 1 (2/2)"
+    "practiceotwo12"  -> "Treino Livre 2 (1/2)"
+    "practiceotwo22"  -> "Treino Livre 2 (2/2)"
+    "practiceothree12"-> "Treino Livre 3 (1/2)"
+    "practiceothree22"-> "Treino Livre 3 (2/2)"
+    "practicesix"     -> "Treino Livre 6"
+    "practiceseven"   -> "Treino Livre 7"
     else              -> this.replaceFirstChar { it.uppercase() }
 }
